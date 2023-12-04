cf503cf504cff61e10124910806ff4c78ea4bbbe
EzerBerdugo <ezerberdugo@gmail.com>
2023-12-04 13:25:55 -0500

    Made changes

--- FILES/sfs_api.h
+++ FILES/sfs_api.h
@@ -1,24 +1,37 @@
 #ifndef SFS_API_H
 #define SFS_API_H
+#include "disk_emu.h"
+#include <stdint.h>
+#define MAXIMUM_FILE_NAME 20
+#define MAXFILENAME MAXIMUM_FILE_NAME
+
+#define FREE(data, bit) \
+    data = data | (1 << bit)
+
+#define USE(data, bit) \
+    data = data & ~(1 << bit)
 
 // You can add more into this file.
 
-void mksfs(int);
+void mksfs(int fresht);
 
-int sfs_getnextfilename(char*);
+int sfs_getnextfilename(char *fname);
 
-int sfs_getfilesize(const char*);
+int sfs_getfilesize(const char* path);
 
-int sfs_fopen(char*);
+int sfs_fopen(char *name);
 
-int sfs_fclose(int);
+int sfs_fclose(int fileID);
 
-int sfs_fwrite(int, const char*, int);
+int sfs_fwrite(int fileID, const char *buf, int length);
 
-int sfs_fread(int, char*, int);
+int sfs_fread(int fileID, char *buf, int length);
 
-int sfs_fseek(int, int);
+int sfs_fseek(int fileID, int loc);
 
-int sfs_remove(char*);
+int sfs_remove(char *file);
 
-#endif
+//HELPER FUNCTIONS
+void set_index_bit(int index);
+int get_index_bit();
+void remove_index_bit(int index);
